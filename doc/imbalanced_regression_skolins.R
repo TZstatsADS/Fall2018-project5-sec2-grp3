f93f0d7c1b31a5aa37fd6faff738b2c783fe8b32
Sam Kolins <sk3651@columbia.edu>
2018-12-02 23:44:54 -0500

    Update imbalanced_regression_skolins.R

--- doc/imbalanced_regression_skolins.R
+++ doc/imbalanced_regression_skolins.R
@@ -13,8 +13,18 @@ library(dplyr)
 train.rev <- train$totals.transactionRevenue
 train.rev[which(is.na(train.rev))] <- 0
 train.rev
-# selecting model x variables (it's just all of them for now)
-x.full <- train %>% select(-totals.transactionRevenue)
+# selecting model x variables (removing NA's)
+# finding NA's by percentage of rows with missing values
+apply(train, 2, function(x) sum(is.na(x)) / length(x) * 100)
+# selecting non-Y columns with fewer than half NA's or is a "totals" column
+x.full <- train[, unname(colMeans(is.na(train)) < 0.5) | grepl("totals", names(train))]
+# replacing NA's in "totals" columns with 0
+x.totals <- x.full[, 9:13]
+x.totals <- apply(x.totals, 2, function(x) replace(x, is.na(x), 0))
+x.full[, 9:13] <- x.totals
+# head(x.full, 5)
+# formatting into a matrix
+x.fullmat <- model.matrix(totals.transactionRevenue ~ ., data = x.full)
 
 # LASSO regression w/ cross-validation (alpha = 1)
 fit1 <- cv.glmnet(x.full, train.rev, alpha = 1)
