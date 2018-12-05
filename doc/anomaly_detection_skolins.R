1cfe6154dd590ff0a179ec93c888f55245065a80
Sam Kolins <sk3651@columbia.edu>
2018-12-04 21:48:44 -0500

    Update anomaly_detection_skolins.R

--- doc/anomaly_detection_skolins.R
+++ doc/anomaly_detection_skolins.R
@@ -10,15 +10,34 @@ library(devtools)
 # install_github("twitter/AnomalyDetection")
 library(AnomalyDetection)
 
-# loading preprocessed data
-load("./output/cleaned.Rdata")
-# saved as "dat"
-# revenue is log revenue of the original data set
+# loading training data
+load("./data/train.Rdata")
+# saved as "train"
 
-# creating a test set from "dat" (which is all training data of the original data set)
+# creating a validation set from "train"
 set.seed(100)
-samp <- sample(nrow(dat), 0.7 * nrow(dat), replace = FALSE)
-train <- dat[samp, ]
-test <- dat[-samp, ]
-summary(train)
-summary(test)
+samp <- sample(nrow(train), 0.7 * nrow(train), replace = FALSE)
+subtrain <- train[samp, ]
+valid <- train[-samp, ]
+summary(subtrain)
+summary(valid)
+
+# the detection function, AnomalyDetectionTs(), takes a 2-column dataframe of timestamps and
+# univariate numerical data, so we need to preprocess our data a little
+times.st <- as.POSIXct(subtrain$visitStartTime, origin = "1970-01-01 00:00.00 UTC")
+revenue.st <- subtrain$totals.transactionRevenue
+revenue.st[which(is.na(revenue.st))] <- 0
+logrev.st <- ifelse(revenue.st != 0, log(revenue.st), 0)
+anomtable.st <- cbind(times.st, logrev.st)
+colnames(anomtable.st) <- c("Times", "Revenue")
+head(anomtable.st)
+tail(anomtable.st)
+
+times.v <- as.POSIXct(valid$visitStartTime, origin = "1970-01-01 00:00.00 UTC")
+revenue.v <- valid$totals.transactionRevenue
+revenue.v[which(is.na(revenue.v))] <- 0
+logrev.v <- ifelse(revenue.v != 0, log(revenue.v), 0)
+anomtable.v <- cbind(times.v, logrev.v)
+colnames(anomtable.v) <- c("Times", "Revenue")
+head(anomtable.v)
+tail(anomtable.v)
