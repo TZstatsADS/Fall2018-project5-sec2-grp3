295973fc0ae10117fccde77e07d785fb1ec50951
Atishay Sehgal <as5453@columbia.edu>
2018-12-05 16:11:31 -0500

    xgboosted classification trees adding more rdata files

--- doc/XgBoosted_Classification_trees.R
+++ doc/XgBoosted_Classification_trees.R
@@ -1,4 +1,4 @@
-# Different Classfication Tree Models to run for Step 1
+# Different XgBoost Models to run for Step 1
 
 # 1) Classfication Tree
 
@@ -6,7 +6,8 @@ library(caret)
 library(tidyverse)
 library(zoo)
 library(DMwR)
-library(rpart)
+library(ROSE)
+library(xgboost)
 
 # Splitting the data into training and test
 set.seed(100)
@@ -14,20 +15,31 @@ samp <- sample(nrow(dat), 0.7*nrow(dat), replace = FALSE)
 train <- dat[samp,]
 test <- dat[-samp,]
 
+# Creating the grid of tuning parameters
+grid_default <- expand.grid(
+  nrounds = 100,
+  max_depth = 10,
+  eta = 0.3,
+  gamma = 0,
+  colsample_bytree = 1,
+  min_child_weight = 1,
+  subsample = 1
+)
+
+
 # Fitting the model
 set.seed(100)
-cltree <- train(is.paid ~ .,
+xgbtree <- train(factor(is.paid) ~ .,
                 data = train[,-1],
-                parms = list(split = "gini"),
-                method = "rpart",
+                tuneGrid = grid_default,
+                method = "xgbTree",
                 trControl = trainControl(method = "cv"))
 
-
 # Predicting the class labels for the test set
-ct.pred <- predict(cltree, newdata = test[,-1])
+xgb.pred <- predict(xgbtree, newdata = test[,-1])
 
 # Confusion Matrix for the predicted classes (positive class is is.paid = 1)
-conf <- table(ct.pred, test$is.paid); conf
+conf <- table(xgb.pred, test$is.paid); conf
 
 # Precision (true pos/true pos + false pos)
 prec <- conf[2,2]/sum(conf[2,]); prec
@@ -46,18 +58,17 @@ rose_train <- ROSE(is.paid ~ ., data  = data.frame(train))$data
 
 # Fitting the model
 set.seed(100)
-rose.cltree <- train(factor(is.paid) ~ .,
-                data = rose_train[,-1],
-                parms = list(split = "gini"),
-                method = "rpart",
-                trControl = trainControl(method = "cv"))
-
+rose.xgbtree <- train(factor(is.paid) ~ .,
+                 data = rose_train[,-1],
+                 tuneGrid = grid_default,
+                 method = "xgbTree",
+                 trControl = trainControl(method = "cv"))
 
 # Predicting the class labels for the test set
-rose.ct.pred <- predict(rose.cltree, newdata = test[,-1])
+rose.xgb.pred <- predict(rose.xgbtree, newdata = test[,-1])
 
 # Confusion Matrix for the predicted classes (positive class is is.paid = 1)
-rose.conf <- table(rose.ct.pred, test$is.paid); rose.conf
+rose.conf <- table(rose.xgb.pred, test$is.paid); rose.conf
 
 # Precision (true pos/true pos + false pos)
 rose.prec <- rose.conf[2,2]/sum(rose.conf[2,]); rose.prec
@@ -77,18 +88,17 @@ smote_train <- SMOTE(is.paid ~ ., data  = data.frame(train))
 
 # Fitting the model
 set.seed(100)
-smote.cltree <- train(factor(is.paid) ~ .,
-                     data = smote_train[,-1],
-                     parms = list(split = "gini"),
-                     method = "rpart",
-                     trControl = trainControl(method = "cv"))
-
+smote.xgbtree <- train(factor(is.paid) ~ .,
+                 data = smote_train[,-1],
+                 tuneGrid = grid_default,
+                 method = "xgbTree",
+                 trControl = trainControl(method = "cv"))
 
 # Predicting the class labels for the test set
-smote.ct.pred <- predict(smote.cltree, newdata = test[,-1])
+smote.xgb.pred <- predict(smote.xgbtree, newdata = test[,-1])
 
 # Confusion Matrix for the predicted classes (positive class is is.paid = 1)
-smote.conf <- table(smote.ct.pred, test$is.paid); smote.conf
+smote.conf <- table(smote.xgb.pred, test$is.paid); smote.conf
 
 # Precision (true pos/true pos + false pos)
 smote.prec <- smote.conf[2,2]/sum(smote.conf[2,]); smote.prec
@@ -100,14 +110,13 @@ smote.rec <- smote.conf[2,2]/sum(smote.conf[,2]); smote.rec
 smote.f1 <- 2*(smote.rec*smote.prec)/(smote.rec+smote.prec); smote.f1
 
 
-
 # Summary
 
-eval.sum <- data.frame(method = c("Normal","ROSE","SMOTE"),
+xgbct.eval.sum <- data.frame(method = c("Normal","ROSE","SMOTE"),
                        precision = c(prec,rose.prec,smote.prec),
                        recall = c(rec,rose.rec,smote.rec),
                        f1score = c(f1,rose.f1,smote.f1))
 
-save(eval.sum, file = "./data/cltree-eval.Rdata")
+save(xgbct.eval.sum, file = "./data/xgbtree-eval.Rdata")
 
 
