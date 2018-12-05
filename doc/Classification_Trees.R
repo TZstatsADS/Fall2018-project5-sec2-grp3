f5463cec6061490d0de09eda9c7cf310cd95bac5
Atishay Sehgal <as5453@columbia.edu>
2018-12-05 03:32:49 -0500

    Making character columns into factors Editing Rose sampled classification trees Saving cleaned data after factor conversion

--- doc/Classification_Trees.R
+++ doc/Classification_Trees.R
@@ -1,4 +1,4 @@
-# Different Classfication Models to run for Step 1
+# Different Classfication Tree Models to run for Step 1
 
 # 1) Classfication Tree
 
@@ -38,39 +38,20 @@ conf[2,2]/sum(conf[,2])
 
 # 2) ROSEd classification tree
 
-# ROSEing the data (Converting to factor for rose)
-dat$browser <- factor(dat$browser)
-dat$os <- factor(dat$os)
-dat$device <- factor(dat$device)
-dat$continent <- factor(dat$continent)
-dat$medium <- factor(dat$medium)
-dat$campaign <- factor(dat$campaign)
-dat$channel <- factor(dat$channel)
-dat$source <- factor(dat$source)
-dat$day <- factor(dat$day)
-dat$is.paid <- factor(dat$is.paid)
-
 #Final ROSEd dataset
-rose_train <- ROSE(is.paid ~ ., data  = data.frame(dat[,-1]))$data
-
-
-# Splitting the data into training and test
-set.seed(100)
-samp <- sample(nrow(rose_train), 0.7*nrow(rose_train), replace = FALSE)
-train <- rose_train[samp,]
-test <- rose_train[-samp,]
+rose_train <- ROSE(is.paid ~ ., data  = data.frame(train))$data
 
 # Fitting the model
 set.seed(100)
 rose.cltree <- train(factor(is.paid) ~ .,
-                data = train,
+                data = rose_train[,-1],
                 parms = list(split = "gini"),
                 method = "rpart",
                 trControl = trainControl(method = "cv"))
 
 
 # Predicting the class labels for the test set
-rose.ct.pred <- predict(rose.cltree, newdata = test)
+rose.ct.pred <- predict(rose.cltree, newdata = test[,-1])
 
 # Confusion Matrix for the predicted classes (positive class is is.paid = 1)
 conf <- table(rose.ct.pred, test$is.paid)
